57efa6ebc56e97e6f6448178fa69b7a6649f7955
Ramon Farizel <ramon.farizel@mobrj.com.br>
2020-07-17 17:14:09 -0300

    Second Tenant

---
+++ XFMultiTenant.Android.TenantB/MainActivity.cs
@@ -0,0 +1,13 @@
+﻿using Android.App;
+using Android.Content.PM;
+using XFMultiTenant.Android.Core;
+using XFMultiTenant.Common;
+
+namespace XFMultiTenant.Android.TenantB
+{
+    [Activity(Label = "XFMultiTenant", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : MainActivityBase
+    {
+        protected override ITenant Tenant => Tenants.TenantB.GetTenant();
+    }
+}
